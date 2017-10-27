1a017f3cbe14ad740b4573561682fdea509ccac0
Anna Selezniova <ask.dev@gmail.com>
2017-10-27 11:49:36 +0300

    Update packages

--- test/AnimakitSlider.test.js
+++ test/AnimakitSlider.test.js
@@ -1,13 +1,13 @@
 /* eslint-env jest */
 
-import { shallow }    from 'enzyme';
+import { render }    from 'enzyme';
 
 import React          from 'react';
 import AnimakitSlider from '../lib/AnimakitSlider.js';
 
 describe('<AnimakitSlider />', () => {
   it('should render', () => {
-    const component = shallow(<AnimakitSlider />);
+    const component = render(<AnimakitSlider />);
 
     expect(component).toMatchSnapshot();
   });
